2e43fda46745ed7887f92f60afd055734b95829d
walter vivar <waltervivar1133@gmail.com>
2025-02-06 16:43:04 -0500

    fix: testimoniales

--- PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
+++ PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
@@ -155,7 +155,7 @@ const home = {
 
         if (success) {
           if (testimonios.length > 0) {
-            // Renderizar el t�tulo y el slide de los testimonios
+            // Renderizar el titulo y el slide de los testimonios
             renderTituloTestHome(testimonios[0]);
             renderSlideTestHome(testimonios);
           } else {
@@ -774,7 +774,7 @@ function renderSlidePEmpHome(pemps) {
 }
 function renderTituloTestHome(test) {
   const html = `
-     <div class="col-12">
+            <div class="col-12">
                 <h2>${test.test_Nombre}</h2>
                 <div class="red-linear"></div>
             </div>
@@ -787,19 +787,19 @@ function renderSlideTestHome(testimonios) {
   const testimoniosContador = testimonios.slice(1);
   testimoniosContador.forEach((test) => {
     html += `        
-                    <div class="swiper-slide">
-                    <div class="testomnio_item card border-0 shadow-lg">
-                        <span class="quote">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
-                                <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388q0-.527.062-1.054.093-.558.31-.992t.559-.683q.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 9 7.558V11a1 1 0 0 0 1 1zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612q0-.527.062-1.054.094-.558.31-.992.217-.434.559-.683.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 3 7.558V11a1 1 0 0 0 1 1z" />
-                            </svg>
-                        </span>
-                        <p class="description">${test.test_Descripcion}</p>
-                        <div class="avatar">
-                            <img src="${test.test_UrlImagen}" alt="">
-                        </div>
-                    </div>
-                </div>
+        <div class="swiper-slide">
+        <div class="testomnio_item card border-0 shadow-lg">
+            <span class="quote">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
+                    <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388q0-.527.062-1.054.093-.558.31-.992t.559-.683q.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 9 7.558V11a1 1 0 0 0 1 1zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612q0-.527.062-1.054.094-.558.31-.992.217-.434.559-.683.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 3 7.558V11a1 1 0 0 0 1 1z" />
+                </svg>
+            </span>
+            <p class="description">${test.test_Descripcion}</p>
+            <div class="avatar">
+                <img src="${test.test_UrlImagen}" alt="">
+            </div>
+        </div>
+    </div>
         `;
   });
   $("#slideTestHome").append(html);
