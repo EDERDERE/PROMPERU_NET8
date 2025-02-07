f398a322240d8f7fae5608d0c3a103759a49952a
walter vivar <waltervivar1133@gmail.com>
2025-02-07 11:40:45 -0500

    feat: update home

--- PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
+++ PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
@@ -1172,11 +1172,11 @@ function renderSeccionHome(info) {
         <div class="col-12 col-md-7">
             <div class="video_about rounded-3 overflow-hidden p-0">
                 <iframe width="100%"
-                        height="100%"
-                        src="${info.info_URLVideo}"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
+                height="100%"
+                src="${info.info_URLVideo}"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
             </div>
         </div>
     </div>        
