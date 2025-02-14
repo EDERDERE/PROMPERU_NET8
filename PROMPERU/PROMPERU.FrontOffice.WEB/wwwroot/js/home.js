a2ecaa16ea1ee46a6e671f6852e34c6dce3d12e1
Francis Del Castillo <francisdelca@hotmail.com>
2025-02-14 15:37:35 -0500

    fix: home styles

--- PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
+++ PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
@@ -670,8 +670,8 @@ function renderContactoHome(fcont) {
             <div class="col-12 col-md-7 description">
                 <div class="title">
                     <h3>${fcont.fcon_SubTitulo}</h3>
-                    <p>${fcont.fcon_DescripcionSubTitulo}</p>
-                    <p>
+                    <p class="fs-5">${fcont.fcon_DescripcionSubTitulo}</p>
+                    <p class="d-flex align-items-center gap-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
                             <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
                             <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
@@ -681,8 +681,8 @@ function renderContactoHome(fcont) {
                 </div>
             </div>
             <div class="col-12 col-md-5 data">
-                <h3>${fcont.fcon_SubTituloDos}</h3>
-                <p class="d-flex align-items-center gap-2">
+                <h3 class="mb-4">${fcont.fcon_SubTituloDos}</h3>
+                <p class="d-flex align-items-center gap-2 mb-4">
                     <strong class="d-flex align-items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                             <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
@@ -693,7 +693,7 @@ function renderContactoHome(fcont) {
                        ${fcont.fcon_Correo}
                     </span>
                 </p>
-                <p class="d-flex align-items-center gap-2">
+                <p class="d-flex align-items-center gap-2 mb-4">
                     <strong class="d-flex align-items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
                             <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
@@ -704,7 +704,7 @@ function renderContactoHome(fcont) {
                         ${fcont.fcon_Telefono}
                     </span>
                 </p>
-                <p class="d-flex align-items-center gap-2">
+                <p class="d-flex align-items-center gap-2 mb-4">
                     <strong class="d-flex align-items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
                             <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7 7 0 0 0-.985-.299l.219-.976q.576.129 1.126.342zm1.37.71a7 7 0 0 0-.439-.27l.493-.87a8 8 0 0 1 .979.654l-.615.789a7 7 0 0 0-.418-.302zm1.834 1.79a7 7 0 0 0-.653-.796l.724-.69q.406.429.747.91zm.744 1.352a7 7 0 0 0-.214-.468l.893-.45a8 8 0 0 1 .45 1.088l-.95.313a7 7 0 0 0-.179-.483m.53 2.507a7 7 0 0 0-.1-1.025l.985-.17q.1.58.116 1.17zm-.131 1.538q.05-.254.081-.51l.993.123a8 8 0 0 1-.23 1.155l-.964-.267q.069-.247.12-.501m-.952 2.379q.276-.436.486-.908l.914.405q-.24.54-.555 1.038zm-.964 1.205q.183-.183.35-.378l.758.653a8 8 0 0 1-.401.432z" />
@@ -837,10 +837,12 @@ function renderLogrosHome(logros) {
 }
 function renderFooterHome(foot) {
   const html = `
+  <div class="logo_footer mb-4">
+    <div class="container py-3">
+    <img src="${foot.foot_UrlLogoPrincipal}" alt="" class="img-fluid">
+    </div>
+  </div>
       <div class="container pt-4 pb-5">
-      <div class="logo_footer mb-4">
-        <img src="${foot.foot_UrlLogoPrincipal}" alt="" class="img-fluid">
-      </div>
       <div class="row">
         <div class="col-12 col-md-8 text-white">
           <h4 class="mb-3 fs-5"> ${foot.foot_Nombre}</h4>
@@ -872,7 +874,11 @@ function renderFooterHome(foot) {
               </svg>
             </a>
           </div>
-          <div class="logo_ministerio">
+          
+        </div>
+        <div class="d-flex align-items-end gap-2 justify-content-between">
+          <span class="text-white">Copyright 2024 PROMPERÚ</span>
+<div class="logo_ministerio">
             <img src="${foot.foot_UrlLogoSecundario}" alt="" class="img-fluid">
           </div>
         </div>
