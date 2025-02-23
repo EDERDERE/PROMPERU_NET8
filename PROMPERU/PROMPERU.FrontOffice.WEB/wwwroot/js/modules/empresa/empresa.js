8defa185dda44c99672c1b37bf7dd537bd840996
walter vivar <waltervivar1133@gmail.com>
2025-02-23 13:29:41 -0500

    feat: fix front

--- PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/modules/empresa/empresa.js
+++ PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/modules/empresa/empresa.js
@@ -22,7 +22,30 @@ export async function loadListEmpresas() {
 function setupFilters() {
   $("#inputRegion, #inputTipoEmpresa")
     .off("change")
-    .on("change", () => {
+    .on("change", function () {
+      applyFilters();
+    });
+
+  $("#inputRUC")
+    .off("input")
+    .on("input", function () {
+      let value = $(this).val();
+
+      value = value.replace(/\D/g, "").slice(0, 11);
+      $(this).val(value);
+
+      if (value.length === 11) {
+        applyFilters();
+      }
+    });
+
+  $("#btnClearFilters")
+    .off("click")
+    .on("click", function () {
+      $("#inputRegion").prop("selectedIndex", 0).trigger("change");
+      $("#inputTipoEmpresa").prop("selectedIndex", 0).trigger("change");
+      $("#inputRUC").val("").trigger("input");
+
       applyFilters();
     });
 }
@@ -30,18 +53,26 @@ function setupFilters() {
 function applyFilters() {
   const selectedRegionLabel = $("#inputRegion option:selected").text();
   const selectedTypeLabel = $("#inputTipoEmpresa option:selected").text();
+  const inputRUC = $("#inputRUC").val();
 
   const filteredEmpresas = empresasData.filter((empresa) => {
     let matchRegion = true;
     let matchType = true;
+    let matchRUC = true;
+
     if (selectedRegionLabel && selectedRegionLabel !== "Seleccione su región") {
       matchRegion = empresa.region === selectedRegionLabel;
     }
     if (selectedTypeLabel && selectedTypeLabel !== "Seleccione su tipo") {
       matchType = empresa.tipoEmpresa === selectedTypeLabel;
     }
-    return matchRegion && matchType;
+    if (inputRUC.length === 11) {
+      matchRUC = empresa.egra_RUC === inputRUC;
+    }
+
+    return matchRegion && matchType && matchRUC;
   });
+
   if (filteredEmpresas.length > 0) {
     renderSliderEmpresa(filteredEmpresas);
   } else {
