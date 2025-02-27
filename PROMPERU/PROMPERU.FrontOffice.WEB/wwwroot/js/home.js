878948ef41f97eaf2e1acae21887f8931f9ea7c2
walter vivar <waltervivar1133@gmail.com>
2025-02-27 13:12:10 -0500

    fix: front

--- PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
+++ PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/home.js
@@ -18,8 +18,12 @@ const home = {
       type: "GET",
       url: "/Logro/ListarLogros",
       dataType: "json",
-      success: function ({ success, logros, message }) {
+      success: function ({ success, logros }) {
         console.log("Respuesta de logros:", logros);
+
+        if (logros.length === 0 || logros.length === 1) {
+          $("#seccionLogros").hide();
+        }
 
         const $logrosHome = $("#logrosHome");
         $logrosHome.empty();
@@ -32,12 +36,6 @@ const home = {
               "<p>No hay información de logros disponibles.</p>"
             );
           }
-        } else {
-          Swal.fire({
-            icon: "error",
-            title: "No hay logros disponibles",
-            text: message || "No se encontraron logros.",
-          });
         }
       },
       error: function (status, error) {
@@ -202,7 +200,7 @@ const home = {
         }
       },
       error: function (xhr, status, error) {
-        console.error("Error en AJAX:", status, error); // Log para debugging
+        console.error("Error en AJAX:", status, error);
 
         Swal.fire({
           icon: "error",
@@ -265,6 +263,10 @@ const home = {
 
         const cursos = response.cursos || [];
 
+        if (cursos.length === 0 || cursos.length === 1) {
+          $("#seccionCursos").hide();
+        }
+
         $("#tituloCursoHome, #botonCursoHome, #sliderCursoHome").empty();
 
         if (cursos.length > 0) {
@@ -289,27 +291,24 @@ const home = {
       url: "/Requisito/ListarRequisitos",
       dataType: "json",
       success: function (response) {
-        console.log("Respuesta del servidor:", response);
+        console.log("Respuesta del servidor requisitos:", response);
 
-        if (!response?.success) {
-          Swal.fire({
-            icon: "error",
-            title: "No hay requisitos disponibles",
-            text: response?.message || "No se encontraron requisitos.",
-          });
-          return;
-        }
+      
 
         const requisitos = response.requisitos || [];
 
-        // Limpia los contenedores antes de renderizar
+        console.log(requisitos, 'data en requisitos')
+        if (requisitos.length === 0 || requisitos.length === 1) {
+          $("#seccionRequisitos").hide();
+        }
+
         $(
           "#tituloRequisitoHome, #sliderRequisitoHome",
-          "botonRequisitoHome"
+          "botonRequisitoHome", "#seccionRequisitos"
         ).empty();
 
         if (requisitos.length > 0) {
-          renderTituloRequisitoHome(requisitos[0]); // Se corrigi� el typo en la funci�n
+          renderTituloRequisitoHome(requisitos[0]);
           renderSliderRequisitoHome(requisitos);
           renderBotonRequisitosHome(requisitos[0]);
         } else {
@@ -440,7 +439,7 @@ const home = {
 
           requestAnimationFrame(() => {
             initSwiper(".mySwiper", {
-              direction: "vertical",
+              direction: "horizontal",
               slidesPerView: 1,
               autoplay: {
                 delay: 3000,
@@ -855,7 +854,9 @@ function renderSliderCursoHome(cursos) {
                       <p class="curso_description">${curs.curs_Descripcion}</p>
                       ${loadModality(curs.tipoModalidadList)}
                       <div class="d-flex justify-content-center mt-auto">
-                          <a href="${curs.curs_LinkBoton}" class="button_brochure"  target="_blank">
+                          <a href="${
+                            curs.curs_LinkBoton
+                          }" class="button_brochure"  target="_blank">
                                ${curs.curs_NombreBoton}
                               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-file-pdf" viewBox="0 0 16 16">
                                   <path d="M4.603 12.087a.8.8 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.7 7.7 0 0 1 1.482-.645 20 20 0 0 0 1.062-2.227 7.3 7.3 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.187-.012.395-.047.614-.084.51-.27 1.134-.52 1.794a11 11 0 0 0 .98 1.686 5.8 5.8 0 0 1 1.334.05c.364.065.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.86.86 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.7 5.7 0 0 1-.911-.95 11.6 11.6 0 0 0-1.997.406 11.3 11.3 0 0 1-1.021 1.51c-.29.35-.608.655-.926.787a.8.8 0 0 1-.58.029m1.379-1.901q-.25.115-.459.238c-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361q.016.032.026.044l.035-.012c.137-.056.355-.235.635-.572a8 8 0 0 0 .45-.606m1.64-1.33a13 13 0 0 1 1.01-.193 12 12 0 0 1-.51-.858 21 21 0 0 1-.5 1.05zm2.446.45q.226.244.435.41c.24.19.407.253.498.256a.1.1 0 0 0 .07-.015.3.3 0 0 0 .094-.125.44.44 0 0 0 .059-.2.1.1 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a4 4 0 0 0-.612-.053zM8.078 5.8a7 7 0 0 0 .2-.828q.046-.282.038-.465a.6.6 0 0 0-.032-.198.5.5 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822q.036.167.09.346z" />
@@ -872,19 +873,23 @@ function renderSliderCursoHome(cursos) {
   $("#sliderCursoHome").append(sliderCurso);
 }
 
-function formatDates(startDate, endDate){
+function formatDates(startDate, endDate) {
   const start = dayjs(startDate);
   const end = dayjs(endDate);
-  
-  if(start.month() === end.month()){
-      return `del ${start.date()} al ${end.date()} de ${start.format('MMMM')} del ${end.year()}`;
+
+  if (start.month() === end.month()) {
+    return `del ${start.date()} al ${end.date()} de ${start.format(
+      "MMMM"
+    )} del ${end.year()}`;
   } else {
-      return `del ${start.date()} de ${start.format('MMMM')} al ${end.date()} de ${end.format('MMMM')} del ${end.year()}`;
+    return `del ${start.date()} de ${start.format(
+      "MMMM"
+    )} al ${end.date()} de ${end.format("MMMM")} del ${end.year()}`;
   }
 }
 
 function loadModality(modalities) {
-  let modalityHTML = '';
+  let modalityHTML = "";
   const calendarIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#C3122C" class="bi bi-calendar4-week" viewBox="0 0 16 16">
               <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
               <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
@@ -894,23 +899,24 @@ function loadModality(modalities) {
   <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
   <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
 </svg>`;
-  modalities.reverse().forEach(modality => {
-
-      modalityHTML += `
+  modalities.reverse().forEach((modality) => {
+    modalityHTML += `
       <p class="d-flex align-items-center gap-2 ">
           ${modality.tmod_ID === 1 ? clockIcon : calendarIcon}
           <strong>${modality.tmod_Nombre}:</strong>
-          <span>${modality.fechaInicio ? formatDates(modality.fechaInicio, modality.fechaFin) : 'En cualquier momento'}</span>
-      </p>`
-
+          <span>${
+            modality.fechaInicio
+              ? formatDates(modality.fechaInicio, modality.fechaFin)
+              : "En cualquier momento"
+          }</span>
+      </p>`;
   });
 
   return modalityHTML;
 }
 
-
 var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
+  direction: "horizontal",
   slidesPerView: 1,
   autoplay: {
     delay: 3000,
@@ -918,9 +924,11 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    type: "bullets",
   },
   touchReleaseOnEdges: true,
 });
+
 
 var swiper = new Swiper(".cursos_swiper", {
   direction: "horizontal",
@@ -1144,4 +1152,6 @@ async function cargarTiposEvento() {
       text: "Hubo un problema al cargar los tipos de eventos. Int�ntelo m�s tarde.",
     });
   }
+
+  
 }
