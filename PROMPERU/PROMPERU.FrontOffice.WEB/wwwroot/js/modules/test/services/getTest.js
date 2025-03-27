d8e314dd1061d72cf1152dd1dd446280f88c2602
walter vivar <waltervivar1133@gmail.com>
2025-03-27 18:05:16 -0500

    feat: edit fetchData

--- PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/modules/test/services/getTest.js
+++ PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/modules/test/services/getTest.js
@@ -1,22 +1,28 @@
-  import { fetchData } from "../../../../shared/js/apiService.js";
-  import { store } from "../state.js";
+import { fetchData } from "../../../../shared/js/apiService.js";
 
-    export async function fetchCompanyData(ruc) {
-      const formData = new FormData();
-      formData.append("ruc", ruc);
-      try {
-        const response = await fetchData(
-          "/Test/ConsultarRUC",
-          "POST",
-          formData,
-          true
-        );
+export async function fetchCompanyData(ruc) {
+  try {
+    const response = await fetchData(
+      "/Test/ConsultarRUC",
+      "GET",
+      { ruc },
+      false
+    );
 
-        if (response.success) {
-          store.setState({ test: response.test });
-          return response.test.evaluated;
-        }
-      } catch (error) {
-        console.error(error);
-      }
+    if (!response) {
+      console.error("No se obtuvo respuesta del servidor.");
+      return null;
     }
+
+    if (response.success) {
+      store.setState({ test: response.test });
+      return response.test.evaluated;
+    } else {
+      console.error("Error en la respuesta:", response.message);
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/modules/test/services/postTest.js
+++ PROMPERU/PROMPERU.FrontOffice.WEB/wwwroot/js/modules/test/services/postTest.js
@@ -1,18 +1,18 @@
 ﻿import { fetchData } from "../../../../shared/js/apiService.js";
-import { fetchData2 } from "../../../../shared/js/apiService.js";
+
 export async function saveTestProgress(saveTest) {
-
-    try {
-        const response = await fetchData2("/Test/GuardarProgresoTest", "POST", saveTest);
-        if (response && response.success) {
-            alert("✅ Test creado exitosamente.");        
-        } else {
-            alert("❌ Ocurrió un error al guardar el Test.");
-        }
-    } catch (error) {
-        console.error("❌ Error al guardar el Test:", error);
+  try {
+    const response = await fetchData(
+      "/Test/GuardarProgresoTest",
+      "POST",
+      saveTest
+    );
+    if (response && response.success) {
+      alert("✅ Test creado exitosamente.");
+    } else {
+      alert("❌ Ocurrió un error al guardar el Test.");
     }
+  } catch (error) {
+    console.error("❌ Error al guardar el Test:", error);
+  }
 }
-   
-
-
